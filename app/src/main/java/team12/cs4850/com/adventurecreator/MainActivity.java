4bb1482d9a1a9871d3db5cc8340ff17aa66d0030
SIatKSU <siskende@students.kennesaw.edu>
2018-02-17 12:45:47 -0500

    Initial commit

---
+++ app/src/main/java/team12/cs4850/com/adventurecreator/MainActivity.java
@@ -0,0 +1,13 @@
+package team12.cs4850.com.adventurecreator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
